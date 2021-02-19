4e709d350003dd442bd4e4e483f365a28d0333ea
Jose Antonio Gonzalez Prada <josgonza@redhat.com>
2021-02-19 17:04:46 +0100

    Initial commit

---
+++ apps/fruits-app/main/java/com/redhat/atomic/fruit/FruitResource.java
@@ -0,0 +1,59 @@
+package com.redhat.atomic.fruit;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
+@Path("/fruit")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class FruitResource {
+    Logger logger = Logger.getLogger(FruitResource.class);
+
+    @ConfigProperty(name = "atomic-fruit.welcome-message", defaultValue = "Welcome")
+    String welcome;
+    
+    @GET
+    @Path("welcome")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String welcome() {
+        logger.debug("welcome method is called");
+        return welcome;
+    }
+
+    @GET
+    public List<Fruit> allFruits() {
+        return Fruit.listAll(); 
+    }
+
+    @GET
+    @Path("{season}")
+    public List<Fruit> fruitsBySeason(@PathParam("season") String season) {
+        return Fruit.getAllFruitsForSeason(season);
+    }
+
+    @POST
+    @Transactional
+    public Response saveFruit(Fruit fruit) {
+        // since the FruitEntity is a panache entity
+        // persist is available by default
+        fruit.persist();
+        final URI createdUri = UriBuilder.fromResource(FruitResource.class)
+                        .path(Long.toString(fruit.id))
+                        .build();
+        return Response.created(createdUri).build();
+    }
+}

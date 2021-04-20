1a2204d022c24934df4fc892c011d9527b395aed
cvicens <carlos.vicens.alonso@gmail.com>
2021-04-20 09:25:10 +0200

    stack/devfile added

---
+++ stack/image-env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export REGISTRY=quay.io
+export REGISTRY_USER_ID=atarazana
+export IMAGE_NAME=quarkus-stack
+export IMAGE_VERSION=2.7-5
+export FROM_IMAGE=registry.redhat.io/codeready-workspaces/plugin-java11-rhel8:2.7-5

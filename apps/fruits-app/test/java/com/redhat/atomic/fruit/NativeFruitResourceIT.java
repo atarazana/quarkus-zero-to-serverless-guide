19b2b8c99dcd7dc22818a9da3cd4d46334af7e5b
cvicens <carlos.vicens.alonso@gmail.com>
2021-07-22 09:58:06 +0200

    faulty and deleted

--- apps/fruits-app/test/java/com/redhat/atomic/fruit/NativeFruitResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.atomic.fruit;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeFruitResourceIT extends FruitResourceTest {
-
-    // Execute the same tests but in native mode.
-}
